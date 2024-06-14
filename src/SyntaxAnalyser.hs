82ce91c75f74809321e030443c66c10b27e55f32
t-sasaki915 <netst915@gmail.com>
2024-06-14 18:40:22 +0900

    implement syntaxanalyser

--- src/SyntaxAnalyser.hs
+++ src/SyntaxAnalyser.hs
@@ -5,23 +5,165 @@ module SyntaxAnalyser
     ) where
 
 import ErrorTrace (Tracable(..))
-import Token (Token(..))
+import Token (Token(..), tokenIndex, tokenLetter)
 
-data SyntaxAnalyserError = UnexpectedEOF String Int
+data SyntaxAnalyserError = UnexpectedToken String Int String String
+                         | UnexpectedEOF String Int
 
 instance Tracable SyntaxAnalyserError where
-    place (UnexpectedEOF a b) = (a, b)
+    place (UnexpectedToken a b _ _) = (a, b)
+    place (UnexpectedEOF a b)       = (a, b)
 
-    title (UnexpectedEOF {})  = "Unexpected end of file"
+    title (UnexpectedToken {})      = "Unexpected token"
+    title (UnexpectedEOF {})        = "Unexpected end of file"
 
-    cause (UnexpectedEOF _ _) = ""
+    cause (UnexpectedToken _ _ a b) =
+        "The interpreter was expecting " ++ b ++ ", but what found was " ++ a ++ "."
+    cause (UnexpectedEOF _ _)       = ""
 
 data LispSyntax = VarValue Token
                 | NumValue Token
-                | FunValue Token
+                | StrValue Token
                 | ExecList [LispSyntax]
                 | RawList [LispSyntax]
                 deriving (Eq, Show)
 
+data State = ExpectingOpenParenthesesOrSingleQuote
+           | ExpectingOpenParentheses 
+           | MakingExecList [LispSyntax]
+           | MakingRawList [LispSyntax]
+
 syntaxAnalyse :: String -> [Token] -> Either SyntaxAnalyserError [LispSyntax]
-syntaxAnalyse src tokens = Right []
+syntaxAnalyse src tokens = topLevel ExpectingOpenParenthesesOrSingleQuote 0 []
+    where
+        topLevel :: State -> Int -> [LispSyntax] ->
+                    Either SyntaxAnalyserError [LispSyntax]
+        topLevel state index determined | index >= length tokens =
+            case state of
+                ExpectingOpenParenthesesOrSingleQuote ->
+                    Right determined
+
+                _ ->
+                    Left $ UnexpectedEOF src 0
+        
+        topLevel ExpectingOpenParenthesesOrSingleQuote index determined =
+            let token = tokens !! index in case token of
+                (OpenParentheses _) ->
+                    topLevel (MakingExecList []) index determined
+            
+                (SingleQuote _) ->
+                    topLevel ExpectingOpenParentheses (index + 1) determined
+
+                t ->
+                    unexpectedToken t "'(' or '''"
+        
+        topLevel ExpectingOpenParentheses index determined =
+            let token = tokens !! index in case token of
+                (OpenParentheses _) ->
+                    topLevel (MakingRawList []) (index - 1) determined
+
+                t ->
+                    unexpectedToken t "'('"
+        
+        topLevel (MakingExecList _) index determined =
+            secondLevel ExpectingOpenParenthesesOrSingleQuote index >>=
+                (\(newIndex, program) ->
+                    topLevel
+                        ExpectingOpenParenthesesOrSingleQuote
+                            (newIndex + 1)
+                                (determined ++ [program])
+                )
+
+        topLevel (MakingRawList _) index determined =
+            secondLevel ExpectingOpenParenthesesOrSingleQuote index >>=
+                (\(newIndex, program) ->
+                    topLevel
+                        ExpectingOpenParenthesesOrSingleQuote
+                            (newIndex + 1)
+                                (determined ++ [program])
+                )
+
+        secondLevel :: State -> Int -> Either SyntaxAnalyserError (Int, LispSyntax)
+        secondLevel _ index | index >= length tokens =
+            Left $ UnexpectedEOF src 0
+        
+        secondLevel ExpectingOpenParenthesesOrSingleQuote index =
+            let token = tokens !! index in case token of
+                (OpenParentheses _) ->
+                    secondLevel (MakingExecList []) (index + 1)
+
+                (SingleQuote _) ->
+                    secondLevel ExpectingOpenParentheses (index + 1)
+                
+                t ->
+                    unexpectedToken t "'(' or '''"
+        
+        secondLevel ExpectingOpenParentheses index =
+            let token = tokens !! index in case token of
+                (OpenParentheses _) ->
+                    secondLevel (MakingRawList []) (index + 1)
+
+                t ->
+                    unexpectedToken t "'('"
+        
+        secondLevel (MakingExecList elems) index =
+            let token = tokens !! index in case token of
+                (Identifier _ _) ->
+                    secondLevel (MakingExecList (elems ++ [VarValue token])) (index + 1)
+
+                (Number _ _) ->
+                    secondLevel (MakingExecList (elems ++ [NumValue token])) (index + 1)
+
+                (StringLiteral _ _) ->
+                    secondLevel (MakingExecList (elems ++ [StrValue token])) (index + 1)
+
+                (OpenParentheses _) ->
+                    secondLevel ExpectingOpenParenthesesOrSingleQuote index >>=
+                        (\(newIndex, program) ->
+                            secondLevel
+                                (MakingExecList (elems ++ [program]))
+                                    (newIndex + 1)
+                        )
+                
+                (SingleQuote _) ->
+                    secondLevel ExpectingOpenParenthesesOrSingleQuote index >>=
+                        (\(newIndex, program) ->
+                            secondLevel
+                                (MakingExecList (elems ++ [program]))
+                                    (newIndex + 1)
+                        )
+
+                (CloseParentheses _) ->
+                    Right (index, ExecList elems)
+
+        secondLevel (MakingRawList elems) index =
+            let token = tokens !! index in case token of
+                (Identifier _ _) ->
+                    secondLevel (MakingRawList (elems ++ [VarValue token])) (index + 1)
+
+                (Number _ _) ->
+                    secondLevel (MakingRawList (elems ++ [NumValue token])) (index + 1)
+
+                (StringLiteral _ _) ->
+                    secondLevel (MakingRawList (elems ++ [StrValue token])) (index + 1)
+
+                (OpenParentheses _) ->
+                    secondLevel ExpectingOpenParenthesesOrSingleQuote index >>=
+                        (\(newIndex, program) ->
+                            secondLevel
+                                (MakingRawList (elems ++ [program]))
+                                    (newIndex + 1)
+                        )
+                
+                (SingleQuote _) ->
+                    secondLevel ExpectingOpenParenthesesOrSingleQuote index >>=
+                        (\(newIndex, program) ->
+                            secondLevel
+                                (MakingRawList (elems ++ [program]))
+                                    (newIndex + 1)
+                        )
+
+                (CloseParentheses _) ->
+                    Right (index, RawList elems)
+
+        unexpectedToken t e = Left $ UnexpectedToken src (tokenIndex t) (tokenLetter t) e
--- src/Token.hs
+++ src/Token.hs
@@ -1,4 +1,8 @@
-module Token (Token(..)) where
+module Token
+    ( Token(..)
+    , tokenIndex
+    , tokenLetter
+    ) where
 
 data Token = OpenParentheses Int
            | CloseParentheses Int
@@ -9,9 +13,25 @@ data Token = OpenParentheses Int
            deriving Eq
 
 instance Show Token where
-    show (OpenParentheses n)  = "(" ++ show n ++ ", \"(\")"
-    show (CloseParentheses n) = "(" ++ show n ++ ", \")\")"
-    show (SingleQuote n)      = "(" ++ show n ++ ", \"'\")"
-    show (Identifier n a)     = "(" ++ show n ++ ", Identifier \"" ++ a ++ "\")"
-    show (StringLiteral n a)  = "(" ++ show n ++ ", String \"" ++ a ++ "\")"
-    show (Number n a)         = "(" ++ show n ++ ", Number \"" ++ a ++ "\")"
+    show (OpenParentheses n)  = "(" ++ show n ++ ", '(')"
+    show (CloseParentheses n) = "(" ++ show n ++ ", ')')"
+    show (SingleQuote n)      = "(" ++ show n ++ ", ''')"
+    show (Identifier n a)     = "(" ++ show n ++ ", Identifier '" ++ a ++ "')"
+    show (StringLiteral n a)  = "(" ++ show n ++ ", String '" ++ a ++ "')"
+    show (Number n a)         = "(" ++ show n ++ ", Number '" ++ a ++ "')"
+
+tokenIndex :: Token -> Int
+tokenIndex (OpenParentheses n)  = n
+tokenIndex (CloseParentheses n) = n
+tokenIndex (SingleQuote n)      = n
+tokenIndex (Identifier n _)     = n
+tokenIndex (StringLiteral n _)  = n
+tokenIndex (Number n _)         = n
+
+tokenLetter :: Token -> String
+tokenLetter (OpenParentheses _)  = "'('"
+tokenLetter (CloseParentheses _) = "')'"
+tokenLetter (SingleQuote _)      = "'''"
+tokenLetter (Identifier _ a)     = "Identifier '" ++ a ++ "'"
+tokenLetter (StringLiteral _ a)  = "String '" ++ a ++ "'"
+tokenLetter (Number _ a)         = "Number '" ++ a ++ "'"
