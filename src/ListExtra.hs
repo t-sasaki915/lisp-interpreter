625b6d9a60950ad811840dbe640fe43201453d8c
t-sasaki915 <netst915@gmail.com>
2024-06-18 19:15:29 +0900

    add functions

--- src/ListExtra.hs
+++ src/ListExtra.hs
@@ -1,4 +1,9 @@
-module ListExtra ((!?), replaceFirst, isPrimitive) where
+module ListExtra
+    ( (!?)
+    , replaceFirst
+    , isPrimitive
+    , isEverythingSame
+    ) where
 
 import Data.List (elemIndices)
 
@@ -7,7 +12,12 @@ import Data.List (elemIndices)
           | otherwise      = Just (xs !! i)
 
 isPrimitive :: Eq a => [a] -> Bool
+isPrimitive [] = True
 isPrimitive xs = all (((< 2) . length) . (`elemIndices` xs)) xs
+
+isEverythingSame :: Eq a => [a] -> Bool
+isEverythingSame [] = True
+isEverythingSame xs = all (head xs ==) xs
 
 replaceFirst :: Eq a => [a] -> [a] -> [a] -> [a]
 replaceFirst [] _ _ = []
