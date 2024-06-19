c4d3fb5c71b08ea637762f00e52cd5098b1755f3
Sasaki Touma <netst915@gmail.com>
2024-06-20 02:05:03 +0900

    use lambdacase

--- src/LispInterpreter.hs
+++ src/LispInterpreter.hs
@@ -16,31 +16,30 @@ translateSyntax :: LispState -> [Syntax] ->
                    Except LispError (LispState, [LispData])
 translateSyntax state =
     foldM
-        (\(st, dt) syntax ->
-            case syntax of
-                (InstantList n lst) ->
-                    translateSyntax st lst >>=
-                        (\(st', lst') ->
-                            return
-                                (st', dt ++ [LispList n lst']))
+        (\(st, dt) -> \case
+            (InstantList n lst) ->
+                translateSyntax st lst >>=
+                    (\(st', lst') ->
+                        return
+                            (st', dt ++ [LispList n lst']))
 
-                (LazyList n lst) ->
-                    translateSyntax st lst >>=
-                        (\(st', lst') ->
-                            return
-                                (st', dt ++ [LispLazyList n lst']))
+            (LazyList n lst) ->
+                translateSyntax st lst >>=
+                    (\(st', lst') ->
+                        return
+                            (st', dt ++ [LispLazyList n lst']))
 
-                (NumberRef n x) ->
-                    return (st, dt ++ [LispNumber n x])
+            (NumberRef n x) ->
+                return (st, dt ++ [LispNumber n x])
 
-                (StringRef n s) ->
-                    return (st, dt ++ [LispString n s])
+            (StringRef n s) ->
+                return (st, dt ++ [LispString n s])
 
-                (BoolRef n b) ->
-                    return (st, dt ++ [LispBool n b])
+            (BoolRef n b) ->
+                return (st, dt ++ [LispBool n b])
 
-                (IdentifierRef n i) ->
-                    return (st, dt ++ [LispIdentifier n i])       
+            (IdentifierRef n i) ->
+                return (st, dt ++ [LispIdentifier n i])       
         )
         (state, [])
 
@@ -49,79 +48,78 @@ evaluateLisp :: LispState -> [LispData] ->
                 ExceptT LispError IO (LispState, [LispData])
 evaluateLisp state =
     foldM
-        (\(st, dt) dat -> do
-            case dat of
-                (LispList n []) ->
-                    return (st, dt ++ [LispBool n False])
+        (\(st, dt) -> \case
+            (LispList n []) ->
+                return (st, dt ++ [LispBool n False])
 
-                (LispList n [LispFunction _ _ prog]) -> do
-                    evalList dt prog n st []
+            (LispList n [LispFunction _ _ prog]) -> do
+                evalList dt prog n st []
 
-                (LispList n (LispFunction _ _ prog : args)) ->
-                    evalList dt prog n st args
+            (LispList n (LispFunction _ _ prog : args)) ->
+                evalList dt prog n st args
 
-                (LispList n' [LispIdentifier n lb]) ->
-                    case find (funcFilt lb) (_functions st) of
-                        Just (LispFunction _ _ prog) ->
-                            evalList dt prog n' st []
+            (LispList n' [LispIdentifier n lb]) ->
+                case find (funcFilt lb) (_functions st) of
+                    Just (LispFunction _ _ prog) ->
+                        evalList dt prog n' st []
 
-                        _ ->
-                            throwE $ UndefinedFunction n lb
+                    _ ->
+                        throwE $ UndefinedFunction n lb
 
-                (LispList n' (LispIdentifier n lb : args)) ->
-                    case find (funcFilt lb) (_functions st) of
-                        Just (LispFunction _ _ prog) ->
-                            evalList dt prog n' st args
+            (LispList n' (LispIdentifier n lb : args)) ->
+                case find (funcFilt lb) (_functions st) of
+                    Just (LispFunction _ _ prog) ->
+                        evalList dt prog n' st args
 
-                        _ ->
-                            throwE $ UndefinedFunction n lb
+                    _ ->
+                        throwE $ UndefinedFunction n lb
 
-                (LispList _ [d]) ->
-                    throwE $ UndefinedFunction (lispDataIndex d) (show d)
+            (LispList _ [d]) ->
+                throwE $ UndefinedFunction (lispDataIndex d) (show d)
 
-                (LispList _ (d : _)) ->
-                    throwE $ UndefinedFunction (lispDataIndex d) (show d)
+            (LispList _ (d : _)) ->
+                throwE $ UndefinedFunction (lispDataIndex d) (show d)
 
-                (LispLazyList n []) ->
-                    return (st, dt ++ [LispBool n False])
+            (LispLazyList n []) ->
+                return (st, dt ++ [LispBool n False])
 
-                (LispLazyList n lst) ->
-                    return (st, dt ++ [LispLazyList n lst])
+            (LispLazyList n lst) ->
+                return (st, dt ++ [LispLazyList n lst])
 
-                (LispNumber n x) ->
-                    return (st, dt ++ [LispNumber n x])
+            (LispNumber n x) ->
+                return (st, dt ++ [LispNumber n x])
 
-                (LispString n s) ->
-                    return (st, dt ++ [LispString n s])
+            (LispString n s) ->
+                return (st, dt ++ [LispString n s])
 
-                (LispBool n b) ->
-                    return (st, dt ++ [LispBool n b])
+            (LispBool n b) ->
+                return (st, dt ++ [LispBool n b])
 
-                (LispVariable _ _ v) ->
-                    return (st, dt ++ [v])
+            (LispVariable _ _ v) ->
+                return (st, dt ++ [v])
 
-                (LispFunction n l p) ->
-                    return (st, dt ++ [LispFunction n l p])
+            (LispFunction n l p) ->
+                return (st, dt ++ [LispFunction n l p])
 
-                (LispIdentifier n lb) ->
-                    let
-                        search filt store = find (filt lb) (store st) >>=
-                            (\case
-                                (LispVariable _ _ v) ->
-                                    Just $ return (st, dt ++ [v])
-                                x ->
-                                    Just $ return (st, dt ++ [x])
-                            )
-                    in
-                    fromMaybe
-                        ( fromMaybe
-                            ( fromMaybe
-                                (throwE $ UndefinedIdentifier n lb)
-                                (search funcFilt _functions)
-                            )
-                            (search varFilt _variables)
+            (LispIdentifier n lb) ->
+                let
+                    search filt store = find (filt lb) (store st) >>=
+                        (\case
+                            (LispVariable _ _ v) ->
+                                Just $ return (st, dt ++ [v])
+                            x ->
+                                Just $ return (st, dt ++ [x])
                         )
-                        (search varFilt _localVariables)
+                in
+                fromMaybe
+                    ( fromMaybe
+                        ( fromMaybe
+                            (throwE $ UndefinedIdentifier n lb)
+                            (search funcFilt _functions)
+                        )
+                        (search varFilt _variables)
+                    )
+                    (search varFilt _localVariables)
         )
         (state, [])
     where
