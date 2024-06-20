080610bc426a3976a5004283f5695d2910b2b50b
Sasaki Touma <netst915@gmail.com>
2024-06-21 08:33:30 +0900

    derive show lispdata

--- src/LispData.hs
+++ src/LispData.hs
@@ -11,7 +11,7 @@ data LispData = LispInteger Int Int
               | LispCharacter Int Char
               | LispList Int [LispData]
               | LispQuote LispData
-              deriving (Eq, Show)
+              deriving Eq
 
 treatAsLispBool :: LispData -> Bool
 treatAsLispBool (LispList _ [])    = False
@@ -36,3 +36,15 @@ index (LispString n _)     = n
 index (LispCharacter n _)  = n
 index (LispList n _)       = n
 index (LispQuote d)        = index d
+
+instance Show LispData where
+    show (LispInteger _ n)    = show n
+    show (LispReal _ n)       = show n
+    show (LispRational _ a b) = show a ++ "/" ++ show b
+    show (LispSymbol _ n)     = n
+    show (LispBool _ True)    = "#T"
+    show (LispBool _ False)   = "#F"
+    show (LispString _ s)     = "\"" ++ s ++ "\""
+    show (LispCharacter _ c)  = "#\\" ++ [c]
+    show (LispList _ l)       = "(" ++ unwords (map show l) ++ ")"
+    show (LispQuote d)        = "'" ++ show d
